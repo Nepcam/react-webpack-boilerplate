e290fc54e5852640996a73d7d13307a1201530cd
Nepcam <cameron@aux.network>
2019-03-11 20:40:52 +1300

    fix up element

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -28,15 +28,15 @@ class App extends React.Component {
         <h1 className='title is-3' style={this.state}>Counting </h1>
         <Counter initialValue={initialValue} valueChanged={this.valueChanged} />
       </div>
-      
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <div>
-            <Footer />
-          </div>
-        </div>
-      </footer>
     )
+      // <footer className="footer">
+      //   <div className="content has-text-centered">
+      //     <div>
+      //       <Footer />
+      //     </div>
+      //   </div>
+      // </footer>
+    
   }
 }
 
