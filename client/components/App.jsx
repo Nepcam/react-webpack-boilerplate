f833a8280cc3dbbe88fd00039c79252d9c5de0cf
Nepcam <cameron@aux.network>
2019-03-11 20:57:09 +1300

    fix ups

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Counter from './Counter'
+import Navigation from './Navigation';
 
 const initialValue = 0
 
@@ -37,7 +38,10 @@ class App extends React.Component {
             </div>
           </div>
         </div>
-        
+
+    <div>
+      <Navigation />  
+    </div>    
   }
 }
 
---
+++ client/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div className="title is-1 has-text-centered">
+            <h2>
+                <strong>Nav Man</strong>
+            </h2>
+        </div>
+    )
+}
+
+export default Navigation
